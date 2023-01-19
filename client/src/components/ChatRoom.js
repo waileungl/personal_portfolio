1daf18bd738b14177f83c1ba9a297ea6654bab27
William Liu <110699040+waileungl@users.noreply.github.com>
2023-01-18 23:03:29 -0800

    update server route

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -46,7 +46,7 @@ function Chat() {
         }
 
         console.log(text)
-        axios.post('https://personal-portfolio-mu-azure.vercel.app', { prompt: user_prompt }, {
+        axios.post('https://personal-portfolio-eta-ivory.vercel.app/', { prompt: user_prompt }, {
             headers: {
                 'Content-Type': 'application/json'
             }
