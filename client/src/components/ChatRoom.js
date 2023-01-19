aa05074eb5571029952b971e189dd386edea1318
William Liu <110699040+waileungl@users.noreply.github.com>
2023-01-18 22:53:21 -0800

    change server api route again

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -46,7 +46,7 @@ function Chat() {
         }
 
         console.log(text)
-        axios.post('https://personal-portfolio-server-eta.vercel.app/', { prompt: user_prompt }, {
+        axios.post('https://personal-portfolio-mu-azure.vercel.app', { prompt: user_prompt }, {
             headers: {
                 'Content-Type': 'application/json'
             }
