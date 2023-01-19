076bf32d7eced7ea1a16ef4f8005cbcf0080884e
William Liu <110699040+waileungl@users.noreply.github.com>
2023-01-18 22:00:17 -0800

    update vercel.json again

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.post('/', async (req, res) => {
         // console.log("Here is the prompt", prompt)
         // console.log("Here is the ai", openai)
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: "text-ada-001",
             prompt: `${prompt}`,
             max_tokens: 100,
             temperature: 0.9,
