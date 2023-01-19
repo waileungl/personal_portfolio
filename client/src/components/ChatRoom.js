39f684219fe48db6fec19ecafccd02536d7f5f04
William Liu <110699040+waileungl@users.noreply.github.com>
2023-01-18 22:43:11 -0800

    deployment configuration

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -46,7 +46,7 @@ function Chat() {
         }
 
         console.log(text)
-        axios.post('https://personal-portfolio-server-one.vercel.app/api', { prompt: user_prompt }, {
+        axios.post('https://personal-portfolio-server-one.vercel.app', { prompt: user_prompt }, {
             headers: {
                 'Content-Type': 'application/json'
             }
--- server/index.js
+++ server/index.js
@@ -20,14 +20,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-app.get('/api', async (req, res) => {
-    res.status.send({
+app.get('/', async (req, res) => {
+    res.json({
         message: 'Hello from William!'
     })
 })
 
 
-app.post('/api', async (req, res) => {
+app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
         // console.log("Here is the prompt", prompt)
